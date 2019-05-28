8994f5191d6fd8c97b9fbd654adc42538347dc22
fstqwq <fstqwq@foxmail.com>
2019-05-28 13:05:35 +0800

    regex fix

--- frontend/Akina/Akina/inputchecker.py
+++ frontend/Akina/Akina/inputchecker.py
@@ -16,10 +16,10 @@ def phoneChecker(x):
     return len(x) > 0 and len(x) <= 20 and re.match(r"^\S+$", x) != None
 
 def locChecker(x):
-    return 1
+    return len(x) > 0 and len(x) <= 20 and re.match(r"^\S+$", x) != None
 
 def dateChecker(x):
-    return 1
+    return len(x) == 10 and re.match(r"^((((1[6-9]|[2-9]\d)\d{2})-(0?[13578]|1[02])-(0?[1-9]|[12]\d|3[01]))|(((1[6-9]|[2-9]\d)\d{2})-(0?[13456789]|1[012])-(0?[1-9]|[12]\d|30))|(((1[6-9]|[2-9]\d)\d{2})-0?2-(0?[1-9]|1\d|2[0-8]))|(((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))-0?2-29))$", x) != None
 
 def catalogChecker(x):
-    return 0
+    return len(x) < 10 and re.match(r"^(?!.*?([CDGKOTZ]).*?\1.*?$)[CDGKOTZ]+$", x) != None
